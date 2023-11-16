9366ca64bd95503caf383901eb1b679ed4635113
SelivanovVyacheslav <selivanow2000@gmail.com>
2023-11-16 16:05:51 +0300

    feat(main): add files lab-06

---
+++ labs/lab06/report/lab6-4.asm
@@ -0,0 +1,28 @@
+%include 'in_out.asm'
+SECTION .data
+msg: DB 'Введите значение x: ',0
+rem: DB 'Результат: ',0
+SECTION .bss
+x: RESB 80
+SECTION .text
+GLOBAL _start
+_start:
+mov eax, msg
+call sprintLF
+mov ecx, x
+mov edx, 80
+call sread
+mov eax,x ; вызов подпрограммы преобразования
+call atoi ; ASCII кода в число, `eax=x`
+add  eax,11
+mov ebx,2 
+mul ebx
+add eax,-6 ;
+mov edi,eax
+
+mov eax,rem ; вызов подпрограммы печати
+call sprint ; сообщения 'Результат: '
+mov eax,edi ; вызов подпрограммы печати значения
+call iprintLF ; из 'edi' в виде символов
+
+call quit ; вызов подпрограммы завершения
