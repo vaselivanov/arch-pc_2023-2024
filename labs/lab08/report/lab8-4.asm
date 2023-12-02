a772f36ee6204a2f49c931c037aaf1b9fe787496
SelivanovVyacheslav <selivanow2000@gmail.com>
2023-12-02 22:21:17 +0300

    feat(main): add files lab08

---
+++ labs/lab08/report/lab8-4.asm
@@ -0,0 +1,26 @@
+%include 'in_out.asm'
+SECTION .data
+msg db "Ответ: ",0
+SECTION .text
+global _start
+_start:
+pop ecx
+pop edx
+sub ecx,1
+mov esi, 0
+next:
+cmp ecx,0h
+jz _end
+pop eax
+call atoi
+mov ebx,2
+mul ebx
+add eax,7
+add esi,eax
+loop next
+_end:
+mov eax, msg
+call sprint
+mov eax, esi
+call iprintLF
+call quit
